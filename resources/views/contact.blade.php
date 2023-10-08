d1b6a176d74615d5310813a307f5bc01ab3eb98d
ranjeet2023 <tdp.ranjeet@gmail.com>
2023-10-09 00:19:52 +0530

    project

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -16,7 +16,7 @@
 @section('content')
     <section data-bs-version="5.1" class="features6 cid-tlVYUtaSS3" id="features06-b" style='background-color:#c6ffe0 !important'>
         <div class="container text-center" >
-            <h2 ><a href="tel:+7217645059" class="text-dark text-center">+91-7217645059</a></h2>
+            <h2 ><a href="tel:+7217645059" class="text-dark text-center">Contact Us for Anything - We're Here in the World of Technology and Communication. <br>+91-7217645059</a></h2>
             <div class="card-wrapper  p-0" >
                 {{-- <div  data-wow-delay="0.3s"> --}}
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d223.7985425360502!2d81.82028533886783!3d26.171401514888387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399af53e8b238743%3A0xa0ad07a64dde17f7!2samethitech!5e0!3m2!1shi!2sin!4v1696785884585!5m2!1shi!2sin" width="auto" height="600px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
